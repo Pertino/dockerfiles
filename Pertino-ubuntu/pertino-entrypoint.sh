08646b7629ca9fd004ae57f55e506720ca6e0f5a
shankins <shankins@pertino.com>
2015-03-03 23:00:52 +0000

    Updated support for latest pertino client

--- Pertino-ubuntu/pertino-entrypoint.sh
+++ Pertino-ubuntu/pertino-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-
 # Configure Pertino -------------------------------------------------------
 echo "Setting up Pertino Client..."
 
@@ -8,14 +7,25 @@ echo "Setting up Pertino Client..."
 touch /etc/sudoers
 mkdir -p /etc/resolvconf
 touch /etc/resolvconf/interface-order
+
+# Known issue with AppArmor
+# See https://github.com/docker/docker/issues/5490
 mv /sbin/dhclient /usr/sbin/dhclient
 
 cd /opt/pertino/pgateway
 if [ ! -z $PERTINO_USERNAME ] && [ ! -z $PERTINO_PASSWORD ]; then
     ./.pauth -u ${PERTINO_USERNAME} -p ${PERTINO_PASSWORD}
 fi
-/etc/init.d/pgateway start
-echo -n 'Starting Tunnel...'; sleep 1;
+
+PERTINO_DAEMON="pGateway"
+if [ -f $PERTINO_DAEMON ];
+then
+   echo -n 'Starting Tunnel...'; sleep 1;
+   ./$PERTINO_DAEMON >/dev/null 2>&1 &
+else
+   echo "File $PERTINO_DAEMON does not exist."
+fi
+
 while STATE=$(/opt/pertino/pgateway/pertino --status | grep -c TUNNEL_RUNNING); test "$STATE" != "1"; do
     echo -n '.'; sleep 1;
 done; echo
