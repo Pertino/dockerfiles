af5773aaf3596dc76e1392ed765b3bdef40d6e6a
shankins <shankins@pertino.com>
2015-03-05 23:59:27 +0000

    Moved paths and commands into env vars

--- Pertino-vnet/entrypoint.sh
+++ Pertino-vnet/entrypoint.sh
@@ -7,29 +7,34 @@ echo "Setting up Pertino Client..."
 touch /etc/sudoers
 touch /etc/resolvconf/interface-order
 
+PERTINO_PATH="/opt/pertino/pgateway"
+PERTINO_AUTHENTICATION_CMD="${PERTINO_PATH}/.pauth"
+PERTINO_CMD="${PERTINO_PATH}/pertino"
+PERTINO_DAEMON="${PERTINO_PATH}/pGateway"
+
 if [ ! -z $PERTINO_APIKEY ]; then
-    /opt/pertino/pgateway/.pauth -a ${PERTINO_APIKEY}
+    $PERTINO_AUTHENTICATION_CMD -a ${PERTINO_APIKEY}
 elif [ ! -z $PERTINO_USERNAME ] && [ ! -z $PERTINO_PASSWORD ]; then
-    /opt/pertino/pgateway/.pauth -u ${PERTINO_USERNAME} -p ${PERTINO_PASSWORD}
+    $PERTINO_AUTHENTICATION_CMD -u ${PERTINO_USERNAME} -p ${PERTINO_PASSWORD}
 fi
 
 echo -n 'Starting Tunnel...'; sleep 1;
 
 # Start the pertino client
-/opt/pertino/pgateway/pGateway >/dev/null 2>&1 &
+$PERTINO_DAEMON >/dev/null 2>&1 &
 
-while STATE=$(/opt/pertino/pgateway/pertino --status | grep -c TUNNEL_RUNNING); test "$STATE" != "1"; do
+while STATE=$($PERTINO_CMD --status | grep -c TUNNEL_RUNNING); test "$STATE" != "1"; do
     echo -n '.'; sleep 1;
 done; echo
 
 
 # If a specific network as been defined, switch to the desirend network
 if ! [ "${PERTINO_NETWORK}" == "" ]; then
-    /opt/pertino/pgateway/pertino --select-network ${PERTINO_NETWORK}
+    $PERTINO_CMD --select-network ${PERTINO_NETWORK}
 fi
 
 # Print out some details about the network configuration and status
-/opt/pertino/pgateway/pertino --status
+$PERTINO_CMD --status
 echo
 ifconfig
 # end configure Pertino ---------------------------------------------------
